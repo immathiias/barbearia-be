6ec085663767feb006e9cee332f5bd4664cead8b
Manoel Mathias <dev.mathiasx@gmail.com>
2023-11-21 10:12:03 -0300

    implementa camada domain

---
+++ src/main/java/br/com/mathias/barbearia/agendamento/domain/Agendamento.java
@@ -0,0 +1,36 @@
+package br.com.mathias.barbearia.agendamento.domain;
+
+import br.com.mathias.barbearia.cortes.domain.Corte;
+import br.com.mathias.barbearia.usuario.domain.Usuario;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Document(collection = "Agendamento")
+public class Agendamento {
+    @Id
+    private UUID idAgendamento;
+    @NotNull
+    private Usuario usuario;
+    @NotNull
+    private Corte corte;
+    @NotNull
+    private LocalDateTime dataHora;
+
+    private LocalDateTime dataHoraDoCadastro;
+    private LocalDateTime dataHoraUltimaAtualizacao;
+}
+
+
+
+
